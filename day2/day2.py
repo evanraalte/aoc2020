bd2f6b5a072b632f3b83f5a6f6db5aea9a3a1eba
Erik van Raalte <erik.vanraalte94@gmail.com>
2020-12-02 10:10:51 +0100

    Create day2.py

---
+++ day2/day2.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+def part1(d):
+    return len(list(filter(lambda x : int(x[0]) <= x[3].count(x[2]) <= int(x[1]),d)))
+
+def part2(d):
+    return len(list(filter(lambda x : (x[3][int(x[0])-1] == x[2]) ^ (x[3][int(x[1])-1] == x[2]),d)))
+
+ex_path     = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(ex_path,"day2.txt")) as f:
+    d = list(map(lambda x: re.search(r'(\d+)-(\d+) ([a-z]): ([a-z]+)',x).groups(),f.read().splitlines()))
+    print(f"Part 1: {part1(d)}")
+    print(f"Part 2: {part2(d)}")
