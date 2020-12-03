7ae9636dbfc81890c242b16ea657ed1fb294835e
Erik van Raalte <erik.vanraalte94@gmail.com>
2020-12-03 10:40:29 +0100

    Update day3 to one liner :)

--- day3/day3.py
+++ day3/day3.py
@@ -2,20 +2,24 @@ import os
 import re
 import functools
 import operator
-def traverse(d, v, p = (0,0)):
-    vx,vy = v
-    px,py = p
 
-    len_line = len(d[0])
-    is_tree = lambda c : d[ c[0] ][ (c[1] % len_line) ] == '#'
+def traverse(d, v):
+    return len([x for x in[ (d[v[1]*i][v[0]*i % len(d[0])]) for i in range(0,round(len(d)/v[1]))] if x == '#'])
 
-    num_trees = 0
-    while py < len(d): # while not all the way down:
-        if is_tree((py,px)):
-            num_trees += 1
-        px += vx
-        py += vy
-    return num_trees
+# def traverse(d, v, p = (0,0)):
+#     vx,vy = v
+#     px,py = p
+
+#     len_line = len(d[0])
+#     is_tree = lambda c : d[ c[0] ][ (c[1] % len_line) ] == '#'
+
+#     num_trees = 0
+#     while py < len(d): # while not all the way down:
+#         if is_tree((py,px)):
+#             num_trees += 1
+#         px += vx
+#         py += vy
+#     return num_trees
 
 def part1(d):
     return traverse(d,(3,1))
