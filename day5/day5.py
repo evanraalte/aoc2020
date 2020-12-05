de7641293571a979b61afa30ab624cd99364def7
Erik van Raalte <erik.vanraalte94@gmail.com>
2020-12-05 11:09:51 +0100

    Add day 5

---
+++ day5/day5.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+def part1(d):
+    return max([mul(e) for e in d])
+    pass
+def part2(d):
+    rows = [e[0] for e in d]
+    cols = [e[1] for e in d]
+    seats = {}
+    for r in range(min(rows)+1,max(rows)-1+1):
+        for c in range(min(cols),max(cols)+1):
+            seats[(r,c)] = ((r,c) in d)
+    s =  list(seats.keys())[list(seats.values()).index(False)]
+    return mul(s)
+
+def to_row(s):
+    return int(s.replace('F','0').replace('B','1'),2)
+
+def to_col(s):
+    return int(s.replace('L','0').replace('R','1'),2)
+
+def mul(t):
+    return t[0]*8+t[1]
+
+ex_path     = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(ex_path,"day5.txt")) as f:
+    d = [(to_row(a),to_col(b)) for x in f.read().split("\n") for (a,b) in re.findall(r'([FB]{7})([LR]{3})', x) ]
+    print(f"Part 1: {part1(d)}")
+    print(f"Part 2: {part2(d)}")
+    pass
