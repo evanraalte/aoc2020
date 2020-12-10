c97843510ad34e9e64b3f2b9cd60878b63ae3185
Erik van Raalte <erik.vanraalte94@gmail.com>
2020-12-10 12:58:43 +0100

    Add function caching

--- day10/time.py
+++ day10/time.py
@@ -1,5 +1,5 @@
 import timeit
 import os
 ex_path     = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(ex_path,"day9.py")) as f:
+with open(os.path.join(ex_path,"day10.py")) as f:
     print (timeit.timeit(stmt = f.read(), number = 100) / 100 ) 
